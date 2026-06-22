0113fbc1ddcd97c193f0d75e6e4ed8340fe9826e
keshavsoft <nknnkumar@yahoo.com>
2026-06-22 10:36:10 +0530

    public 4

--- src/nav.js
+++ src/nav.js
@@ -1,8 +1,8 @@
 // src/header.js
-import initNav from "../bin/commands/template/v3/index.js";
+import initNav from "../bin/commands/template/v4/index.js";
 
 (async () => {
-    window.KSCompNavVersion = "v3.1";
+    window.KSCompNavVersion = "v4.1";
 
     window.KSCompNav = initNav;
 })();
