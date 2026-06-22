16c9c00fbd5f0ee87c157fc2e724ee25f3f8188f
keshavsoft <nknnkumar@yahoo.com>
2026-06-22 09:21:58 +0530

    KeshavSoft

---
+++ src/header.js
@@ -0,0 +1,8 @@
+// src/header.js
+import initHeader from "../bin/header/v5/commands/header/template/v8/initHeader.js";
+
+(async () => {
+    window.KSHeaderVersion = "v5.8";
+
+    window.KSHeader = initHeader;
+})();
